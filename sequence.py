733728678fa1928bc6d561182c857e6d6ebfa75a
Pritindra <37242669+pritindra@users.noreply.github.com>
2020-09-05 08:55:32 +0530

    Update sequence.py

--- sequence.py
+++ sequence.py
@@ -9,20 +9,42 @@ def split(l):
 patt_list = split(patt)
 inp_list = split(inp)
 
-
 temp = []
-for p in range(len(patt_list)):
+
+choice = input("Do you want to print output in a text file ?[y/n]")
+
+if choice == "y":
+    file = open('output.txt', 'w')
+    sys.stdout = file
+    for p in range(len(patt_list)):
+                
+        for s in range(len(patt_list)):
+            temp.insert(s,patt_list[s])
+
+        for q in range(len(inp_list)):
             
-    for s in range(len(patt_list)):
-        temp.insert(s,patt_list[s])
+            st = ""
+            temp[p] = inp_list[q]
+            print(st.join(temp))
+        del temp[:]
 
-    for q in range(len(inp_list)):
+    output = (p+1)*(q+1)
+    print("No. of Outputs::" + str(output))
         
-        st = ""
-        temp[p] = inp_list[q]
-        print(st.join(temp))
-    del temp[:]
+    file.close()
 
-output = (p+1)*(q+1)
-print("No. of Outputs::" + str(output))
-    
+else:
+    for p in range(len(patt_list)):
+                
+        for s in range(len(patt_list)):
+            temp.insert(s,patt_list[s])
+
+        for q in range(len(inp_list)):
+            
+            st = ""
+            temp[p] = inp_list[q]
+            print(st.join(temp))
+        del temp[:]
+
+    output = (p+1)*(q+1)
+    print("No. of Outputs::" + str(output))

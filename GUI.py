e891806db6eb0483c92e968f957455d38723d60c
Pritindra <37242669+pritindra@users.noreply.github.com>
2020-09-06 09:36:09 +0530

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -14,7 +14,8 @@ def sequence():
 
 
     temp = []
-    
+    z = []
+
     for p in range(len(patt_list)):
                 
         for s in range(len(patt_list)):
@@ -22,40 +23,48 @@ def sequence():
 
         for q in range(len(inp_list)):
             
-            str = ""
+            st = ""
             temp[p] = inp_list[q]
-            z1 = z1 + str.join(temp)
+            z1 = st.join(temp)
+            z.append(z1)   
         del temp[:]
 
+    for i in range(len(z)):
+        t1.insert(END,str(z[i]) + "\n")
+        # labels[i].place(x=20,y=30+(20*i))
     
-    L=Label(root, text=z1)
-    L.grid(row = 1,column = 1)
+   
 
+if __name__=="__main__":
+    root = Tk()
+    root.title("Sequence Builder")
+    root.config(bg="gray85")
 
-root = Tk()
-root.title("Sequence Builder")
-root.config(bg="gray85")
+    pattern = StringVar()
+    inputs = StringVar()
 
-pattern = StringVar()
-inputs = StringVar()
+    # frame = Frame(root, width=600, height=200, bg="dark sea green")
+    # frame.grid(row=0,column=0,padx=10,pady=5)
 
-frame = Frame(root, width=600, height=200, bg="dark sea green")
-frame.grid(row=0,column=0,padx=10,pady=5)
+    # c = Canvas(root, height=400, width=600, bg="gray78")
+    # c.grid(row=1,column=0, padx=5, pady=5)
 
-c = Canvas(root, height=400, width=600, bg="gray78")
-c.grid(row=1,column=0, padx=5, pady=5)
+    l1=Label(root,text="Pattern")
+    l1.place(x=0,y=0)
 
-e1= Entry(root,textvariable=pattern,font=('calibre',10,'normal')) 
-e1.grid(row=0,column=0,padx=5,pady=5)
+    l2=Label(root,text="Inputs")
+    l2.place(x=0,y=30)
 
-e2= Entry(root,textvariable=inputs,font=('calibre',10,'normal')) 
-e2.grid(row=0,column=1,padx=5,pady=5)
+    e1=Entry(root,textvariable=pattern)
+    e1.place(x=120,y=0)
 
-button = Button(root, text = 'Enter', width = 10,
-    command = sequence)
-button.grid(row = 0, column = 2)
+    e2=Entry(root,textvariable=inputs)
+    e2.place(x=120,y=30)
 
-# max_val = Scale(frame,from_=80, to=100,length=100,resolution=1,orient=HORIZONTAL,label="Max. value",activebackground="lavender")
-# max_val.grid(row=0,column=1,padx=5,pady=5)
-# max_val.config(bg="LightCyan2")
-root.mainloop()
+    b1=Button(root,text="Enter",command=sequence,width=30,bg="light slate blue",activebackground="slate blue")
+    b1.grid(row=11,column=0,padx=10)
+
+    t1=Text(root,width=80,height=20)
+    t1.grid(row=0,column=1)
+
+    root.mainloop()
